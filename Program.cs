c5e46340e9dbbeaefb5e69ac5518cd1ef5995cc6
hasanbadr <hasan.badr@edu.keuda.fi>
2023-12-12 08:06:05 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Parittomat luvut välillä 1-99:");
+
+        for (int i = 1; i <= 99; i += 2)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
